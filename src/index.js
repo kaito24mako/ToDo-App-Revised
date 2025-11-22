dca160ab3bdb67aab65c4dc81654c89c19126f44
kaito24mako <kaitowatanabemcc@gmail.com>
2025-11-22 14:25:08 +1100

    create constructor and functin for creating new tasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./index.css";
+
+// const taskInput = document.querySelector('#todo-input');
+// const dateInput = document.querySelector('#todo-date');
+
+const taskArray = [];
+
+class Task {
+    constructor(title, date, completed = false) {
+        this.title = title;
+        this.date = date;
+        this.completed = completed;
+    }
+}
+
+function createTask(title, date, completed) {
+    let newTask = new Task(title, date, completed);
+    taskArray.push(newTask);
+}
+
+console.log(createTask('do project', 'october'))
+console.log(createTask('finish project', 'october'))
+console.log(taskArray);
